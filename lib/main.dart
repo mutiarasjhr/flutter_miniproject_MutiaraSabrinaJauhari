9afed441455b65b8eeb257c976d973720642eb43
mutiarasjhr <mutiarajauhari1910@gmail.com>
2023-05-02 00:10:16 +0700

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:yourmuaa/Screen/home_page.dart';
 import 'package:yourmuaa/Screen/login_page.dart';
+import 'package:provider/provider.dart';
+import 'Screen/bookingmua.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +15,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: {
         '/': (context) => LoginPage(),
         '/home': (context) => HomePage(),
+        // '/booking': (context) => BookingPage(),
+        // '/specialist': (context) => SpecialistPage()
       },
       // home: LoginPage(),
     );
